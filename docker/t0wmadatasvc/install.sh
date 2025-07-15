b1083aaa5def46e091c6d27e11d0c142c492b464
Krittin Phornsiricharoenphant <oatkrittin@gmail.com>
2025-07-15 16:08:01 +0200

    use python -m pip to avoid mismatch.

--- docker/t0wmadatasvc/install.sh
+++ docker/t0wmadatasvc/install.sh
@@ -2,7 +2,8 @@
 #No longer using rpm for packaging t0wmadatasvc
 
 TAG=2.1.1
-pip install --upgrade pip
-pip install wmcore
-pip install --no-cache-dir git+https://github.com/dmwm/t0wmadatasvc.git@$TAG
+WMCORE_VERSION=2.4.1
+python -m pip install --upgrade pip
+python -m pip install wmcore==$WMCORE_VERSION
+python -m pip install --no-cache-dir git+https://github.com/dmwm/t0wmadatasvc.git@$TAG
 

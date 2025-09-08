d0f70faeb4005ce2b0193fe5ca200fa18d9ac856
Stefano Belforte <stefano.belforte@cern.ch>
2025-09-08 16:02:40 +0200

    add overlay for test2 config

---
+++ argocd/apps/crab/crabserver/overlays/test2/config/config.py
@@ -0,0 +1,46 @@
+from WMCore.Configuration import Configuration
+import re
+import socket
+import time
+import os
+
+myhost = socket.getfqdn().lower()
+
+conf = Configuration()
+main = conf.section_('main')
+srv = main.section_('server')
+srv.thread_pool = int(os.getenv('CRABSERVER_THREAD_POOL', 25))
+main.application = 'crabserver'
+main.port = 8270
+main.index = 'data'
+
+main.authz_defaults = { 'role': None, 'group': None, 'site': None }
+main.section_('tools').section_('cms_auth').key_file = "%s/auth/crabserver/header-auth-key" % __file__.rsplit('/', 3)[0]
+
+app = conf.section_('crabserver')
+app.admin = 'cms-service-webtools@cern.ch'
+app.description = 'CRABServer RESTFull API'
+app.title = 'CRABRESTFull'
+
+views = conf.section_('views')
+ui = views.section_('ui')
+ui.object = 'CRABInterface.Pages.FrontPage.FrontPage'
+
+data = views.section_('data')
+data.object = 'CRABInterface.RESTBaseAPI.RESTBaseAPI'
+data.backend = 'oracle'
+data.db = 'CRABServerAuth.dbconfig'
+data.s3 = 'CRABServerAuth.s3'
+data.s3_bucket = 'crabcache_dev'
+data.workflowManager = 'HTCondorDataWorkflow'
+data.compatibleClientVersions = ["v3", "development"]
+
+data.extconfigurl = 'http://gitlab.cern.ch/crab3/CRAB3ServerConfig/raw/master/cmsweb-rest-config-test.json'
+
+data.loggingLevel = 10
+data.loggingFile = '%s/logs/crabserver/CRAB-%s.log' % (__file__.rsplit('/', 4)[0], myhost)
+data.keptLogDays = 7
+
+data.enableQueryLoadAllRows = os.getenv('CRABSERVER_ENABLE_QUERY_LOAD_ALL_ROWS', 'True').lower() in ('true', '1', 't')
+
+data.delegateDN = "/DC=ch/DC=cern/OU=Organic Units/OU=Users/CN=cmscrab/CN=(817881|373708)/CN=Robot: cms crab|/DC=ch/DC=cern/OU=Organic Units/OU=Users/CN=crabint1/CN=373708/CN=Robot: CMS CRAB Integration 1"
